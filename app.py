5bd22d3cc882b9f3b90f52ade036b504ce601974
krishnachauhan0204-collab <krishna.chauhan.0204@gmail.com>
2026-09-09 17:50:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/students")
+def students():
+    return render_template("students.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)v
