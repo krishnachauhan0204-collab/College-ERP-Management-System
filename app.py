8323466a174767156410dde4cae83617498165db
krishnachauhan0204-collab <krishna.chauhan.0204@gmail.com>
2026-09-12 14:16:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,5 +127,8 @@ def faculty():
 @app.route("/courses")
 def courses():
     return render_template("courses.html")
+    @app.route("/attendance")
+def attendance():
+    return render_template("attendance.html")
 if __name__ == "__main__":
     app.run(debug=True)
